51fa9c45f63a0ca8558c4336f362ae9dc0e77829
Roman Parpalak <roman@parpalak.com>
2023-09-25 23:41:28 +0300

    i.upmath.me is now connected via s2_latex extension.

--- written_ru.php
+++ written_ru.php
@@ -8,7 +8,6 @@ return (new AssetPack(__DIR__))
     ->addCss('highlight-js/default.min.css')
     ->addHeadJs('https://www.googletagmanager.com/gtag/js?id=G-LZD7Q0K8SM', [AssetPack::OPTION_ASYNC])
     ->addHeadInlineJs("<script>  window.dataLayer = window.dataLayer || [];\n  function gtag(){dataLayer.push(arguments);}\n  gtag('js', new Date());\n  gtag('config', 'G-LZD7Q0K8SM', {'dimension1': window.devicePixelRatio || '-'});</script>")
-    ->addJs('//i.upmath.me/latex.js', [AssetPack::OPTION_PRELOAD, AssetPack::OPTION_DEFER]) // todo понять, почему не через расширение. чтобы РСС не преобразовывать?
     ->addJs('script.js', [AssetPack::OPTION_MERGE, AssetPack::OPTION_DEFER])
     ->addJs('likely2.8/likely.min.js', [AssetPack::OPTION_MERGE])
     ->addCss('likely2.8/likely.css', [AssetPack::OPTION_MERGE])
